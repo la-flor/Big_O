f7d82729da6be7ec8c607b187c7489d6a832b68e
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-01 14:21:03 -0700

    partially completed linked-list practice

--- Array-LinkedListPractice/linked-list.js
+++ Array-LinkedListPractice/linked-list.js
@@ -21,25 +21,73 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
-
+    const newestNode = new Node(val)
+    if (this.length === 0) {
+      this.head = newestNode;
+      this.tail = newestNode;
+    } else {
+      let currentNode = this.head
+      while (currentNode.next) {
+        currentNode = this.tail
+      }
+      currentNode.next = newestNode;
+      this.tail = newestNode;
+    }
+    this.length += 1;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    const firstNode = new Node(val);
+    if (this.length === 0) {
+      this.head = firstNode;
+      this.tail = firstNode;
+    } else {
+      firstNode.next = this.head;
+      this.head = firstNode;
+    }
+    this.length += 1;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    if (this.length === 0) return undefined;
 
+    const toBePoped = this.tail.val
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let currentNode = this.head;
+      let toBePoped = this.tail.val;
+      while (currentNode.next.val !== toBePoped) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = null;
+      this.tail = currentNode;
+    }
+
+    this.length -= 1;
+    return toBePoped;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    if (this.length === 0) return undefined;
 
+    const toBeRemoved = this.head.val;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+
+    this.length -= 1;
+    return toBeRemoved;
   }
 
   /** getAt(idx): get val at idx. */
@@ -74,3 +122,8 @@ class LinkedList {
 }
 
 module.exports = LinkedList;
+
+let lst = new LinkedList();
+lst.push(3)
+lst.push(5)
+lst.pop() 
