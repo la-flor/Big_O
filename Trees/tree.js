0012e18ee8767f82c459a12b83acb16fe84489e2
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-07 10:01:25 -0700

    Completed sumValues()

--- Trees/tree.js
+++ Trees/tree.js
@@ -15,7 +15,22 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+    if (!this.root) return 0;
+
+    let sum = this.root.val;
     
+    function findChildVals(node) {
+      for (let child of node.children) {
+        sum += child.val;
+
+        if (child.children.length > 0) {
+          findChildVals(child);
+        }
+      }
+    }
+
+    findChildVals(this.root);
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
@@ -31,5 +46,15 @@ class Tree {
 
   }
 }
+
+
+
+emptyTree = new Tree();
+
+let nSmall = new TreeNode(1);
+let nSmall2 = new TreeNode(2);
+nSmall.children.push(nSmall2);
+smallTree = new Tree(nSmall);
+smallTree.sumValues()
 
 module.exports = { Tree, TreeNode };
