a87627a579536543b9735972a9c78c6b37b31491
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-08 16:59:10 -0700

    Completed graph exercises

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -11,25 +11,98 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex)
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    vertexArray.map((newVertex) => this.nodes.add(newVertex));
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    this.nodes.forEach((node) => {
+      if (node.value === v1.value) {
+        node.adjacent.add(v2);
+      }
+      if (node.value === v2.value) {
+        node.adjacent.add(v1);
+      }
+    })
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    this.nodes.forEach((node) => {
+      if (node.value === v1.value) {
+        node.adjacent.delete(v2);
+      }
+      if (node.value === v2.value) {
+        node.adjacent.delete(v1);
+      }
+    })
+  }
 
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  /* this function accepts a vertex and removes it from the nodes property, 
+    it also updates any adjacency lists that include that vertex */
+  removeVertex(vertex) {
+    this.nodes.forEach((node) => {
+      if (node.value === vertex.value) {
+        this.nodes.delete(node);
+      }
+      if (node.adjacent === vertex.value) {
+        node.adjacent.delete(vertex);
+      }
+    })
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const stack = [start];
+    const result = [];
+    const checked = new Set();
+    let current;
+
+    checked.add(start);
+
+    while (stack.length) {
+      current = stack.pop();
+      result.push(current.value);
+
+      current.adjacent.forEach(neighbor => {
+        if (!checked.has(neighbor)) {
+          checked.add(neighbor);
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const stack = [start];
+    const result = [];
+    const checked = new Set();
+    let current;
+
+    checked.add(start);
+
+    while (stack.length) {
+      current = stack.shift();
+      result.push(current.value);
+
+      current.adjacent.forEach(neighbor => {
+        if (!checked.has(neighbor)) {
+          checked.add(neighbor);
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
+
 
 module.exports = {Graph, Node}
