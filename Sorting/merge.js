b549139ddbf2fcb433a4c83fe0668709c89a600b
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-09 10:37:29 -0700

    Completed mergeSort()

--- Sorting/merge.js
+++ Sorting/merge.js
@@ -1,5 +1,39 @@
-function merge() {}
+function merge(arr1, arr2) {
+    // compare first values and merge into 1 array to be returned
+    const results = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+}
+
+function mergeSort(array) {
+    // split down into groups to run merge on
+    if (array.length <= 1) return array;
+
+    const midIdx = Math.floor(array.length / 2);
+    const left = mergeSort(array.slice(0,midIdx));
+    const right = mergeSort(array.slice(midIdx));
+    
+    return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
